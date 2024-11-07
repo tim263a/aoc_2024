5bc447a4a39fcb12d85f875129d22cfe738c1a45
Tim Gengenbach <tim.gengenbach@online.de>
2024-11-07 21:22:59 +0100

    run.sh: read input file and pipe to binary

--- run.sh
+++ run.sh
@@ -7,8 +7,19 @@
     cmake --build .
 )
 
+file_path=inputs/day_$1.txt
+if [ $# -lt 1 ]; then
+    echo "Usage: $0 <day_index>"
+    exit 1
+elif [ -f $file_path ]; then
+    file_content=$(cat $file_path)
+else
+    echo "File $file_path not found."
+    exit 1
+fi
+
 (
     cd build
 
-    ./aoc_wannabe
+    echo $file_content | ./aoc_wannabe
 )
