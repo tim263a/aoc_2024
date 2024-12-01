0854e5322ee91c177a96451be6b26af05e682e49
Tim Gengenbach <tim.gengenbach@online.de>
2024-12-01 10:08:02 +0100

    Extract read_input

---
+++ code/api/util/read_input.h
@@ -0,0 +1,5 @@
+#include <string>
+#include <vector>
+
+std::string read_stdin();
+std::vector<std::string> read_stdin_lines();
--- code/src/main.cpp
+++ code/src/main.cpp
@@ -9,45 +9,7 @@
 #include <string>
 #include <vector>
 
-std::string read_stdin()
-{
-    constexpr size_t BUFFER_SIZE = 1024;
-    char buffer[BUFFER_SIZE];
-
-    std::string input;
-    input.reserve(BUFFER_SIZE);
-
-    while (std::cin.read(buffer, BUFFER_SIZE)) {
-        input.append(buffer, std::cin.gcount());
-    }
-
-    if (std::cin.fail() && !std::cin.eof())
-    {
-        std::cerr << "Error reading input\n";
-        std::exit(1);
-    }
-
-    return input;
-}
-
-std::vector<std::string> read_stdin_lines()
-{
-    std::vector<std::string> result;
-
-    std::string line;
-    while (std::getline(std::cin, line))
-    {
-        result.push_back(line);
-    }
-
-    if (std::cin.fail() && !std::cin.eof())
-    {
-        std::cerr << "Error reading input\n";
-        std::exit(1);
-    }
-
-    return result;
-}
+#include "util/read_input.h"
 
 int main(int argc, char** argv)
 {
