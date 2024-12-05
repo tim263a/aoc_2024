45a0d0e5fd7d9e71c9acab364c7e2a0025c638ae
Tim Gengenbach <tim.gengenbach@cetitec.com>
2024-12-05 17:31:54 +0100

    Fix day04 part 1

--- code/src/days/day04.cpp
+++ code/src/days/day04.cpp
@@ -17,31 +17,35 @@ void Day04::parseInput()
 int_fast16_t rowIdx = 0;
 int_fast16_t columnIdx = 0;
 
-static void checkOrReset(
+static int_fast16_t checkOrReset(
     decltype(42) checkIdx,
     char currentChar, std::string_view target,
-    int_fast16_t& runlength, uint64_t& counter)
+    int_fast16_t runlength, uint64_t& counter)
 {
+    int_fast16_t result = 0;
+
     if (currentChar == target[runlength])
     {
-        runlength += 1;
+        result = runlength + 1;
     }
     else if (currentChar == target[0])
     {
-        runlength = 1;
+        return 1;
     }
     else
     {
-        runlength = 0;
+        return 0;
     }
 
-    if (runlength == 4)
+    if (result == 4)
     {
         printFmt("{} row {} column {}\n", checkIdx, rowIdx, columnIdx);
 
         counter += 1;
-        runlength = 0;
+        result = 0;
     }
+
+    return result;
 }
 
 constexpr std::string_view FORWARDS = "XMAS";
@@ -60,28 +64,28 @@ uint64_t Day04::calculatePart1()
     printFmt("Line Length {}\n", lineLength);
 
     // Runlengths of:
-    // 1 - down left
-    // 2 - up right -> backwards
-    // 3 - down
-    // 4 - up -> backwards
-    // 5 - down right
-    // 6 - up left -> backwards
-    std::array<std::vector<int_fast16_t>, 6> runlengths;
-    for (auto& v : runlengths)
-    {
-        v.resize(lineLength);
-    }
+    // 0 - down right
+    // 1 - up left -> backwards
+    // 2 - down
+    // 3 - up -> backwards
+    // 4 - down left
+    // 5 - up right -> backwards
+    std::vector<std::array<int_fast16_t, 6>> runlengths(lineLength + 2, {{}});
+
     int_fast16_t runLengthForwards = 0;
     int_fast16_t runLengthBackwards = 0;
 
-    std::array<std::size_t, 6> shifts;
-    shifts.fill(4 * lineLength); // make modulo positive
+    std::size_t delayIdx = 0;
+    std::array<int_fast16_t, 6> delayed {};
+    std::size_t delayIdx2 = 0;
+    std::array<int_fast16_t, 6> delayed2 {};
 
-    std::size_t lineIdx = 0;
-    std::size_t idx = 0;
+    std::size_t lineIdx = 1;
+    std::size_t idx = 1;
     for (it = m_buffer.begin(); it != m_buffer.end(); it++, idx++)
     {
-        bool invalidChar = false;
+        rowIdx = lineIdx;
+        columnIdx = idx;
 
         switch (*it)
         {
@@ -89,51 +93,43 @@ uint64_t Day04::calculatePart1()
             case 'M':
             case 'A':
             case 'S':
+                runLengthForwards = checkOrReset(0, *it, FORWARDS, runLengthForwards, sum);
+                runLengthBackwards = checkOrReset(1, *it, BACKWARDS, runLengthBackwards, sum);
+
+                delayIdx = idx;
+                delayed = {
+                    checkOrReset(2, *it, FORWARDS, runlengths[idx + 1][0], sum),
+                    checkOrReset(3, *it, BACKWARDS, runlengths[idx + 1][1], sum),
+                    checkOrReset(4, *it, FORWARDS, runlengths[idx][2], sum),
+                    checkOrReset(5, *it, BACKWARDS, runlengths[idx][3], sum),
+                    checkOrReset(6, *it, FORWARDS, runlengths[idx - 1][4], sum),
+                    checkOrReset(7, *it, BACKWARDS, runlengths[idx - 1][5], sum)
+                };
                 break;
 
             case '\n':
                 lineIdx++;
-                idx = -1;
+                idx = 0; // So next iteration starts with 1.
 
                 runLengthForwards = 0;
                 runLengthBackwards = 0;
-                shifts[0] += 1;
-                shifts[1] += 1;
-                shifts[4] -= 1;
-                shifts[5] -= 1;
+
+                // runlengths.begin()->fill(0);
+                // runlengths.rbegin()->fill(0);
                 break;
 
             default:
-                invalidChar = true;
-                continue;
+                runLengthForwards = 0;
+                runLengthBackwards = 0;
+
+                delayIdx = idx;
+                delayed.fill(0);
+                break;
         }
 
-        std::array<int_fast16_t*, 6> shifted;
-        for (std::size_t i = 0; i < shifted.size(); i++)
-        {
-            shifted[i] = &runlengths[i].at((idx + shifts[i]) % lineLength);
-            if (invalidChar)
-            {
-                *shifted[i] = 0;
-            }
-        }
-
-        if (invalidChar)
-        {
-            continue;
-        }
-
-        rowIdx = lineIdx;
-        columnIdx = idx;
-
-        checkOrReset(0, *it, FORWARDS, runLengthForwards, sum);
-        checkOrReset(1, *it, BACKWARDS, runLengthBackwards, sum);
-        checkOrReset(2, *it, FORWARDS, *shifted[0], sum);
-        checkOrReset(3, *it, BACKWARDS, *shifted[1], sum);
-        checkOrReset(4, *it, FORWARDS, *shifted[2], sum);
-        checkOrReset(5, *it, BACKWARDS, *shifted[3], sum);
-        checkOrReset(6, *it, FORWARDS, *shifted[4], sum);
-        checkOrReset(7, *it, BACKWARDS, *shifted[5], sum);
+        runlengths[delayIdx2] = delayed2;
+        delayIdx2 = delayIdx;
+        delayed2 = delayed;
     }
 
     return sum;
