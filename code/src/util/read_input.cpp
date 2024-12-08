1350b687028faf8b04ae562d54eb2039d8289be2
Tim Gengenbach <tim.gengenbach@online.de>
2024-12-08 17:42:48 +0100

    read_input.cpp: replace read_stdio_into_buffer with read_stdio_into_buffer_unistd

--- code/src/util/read_input.cpp
+++ code/src/util/read_input.cpp
@@ -12,34 +12,6 @@
 
 void read_stdio_into_buffer(std::vector<uint8_t>& buffer)
 {
-    constexpr uint_fast32_t FALLBACK_SIZE { 1024 };
-
-    uint_fast32_t alreadyRead = 0;
-    uint_fast32_t batchSize = buffer.size();
-    while (std::cin.read((char*) buffer.data() + alreadyRead, batchSize)) [[unlikely]]
-    {
-        alreadyRead += batchSize;
-        batchSize = FALLBACK_SIZE;
-        buffer.resize(alreadyRead + batchSize);
-
-#ifdef NDEBUG
-        std::cerr << "Warning: Input buffer resize was required " <<
-            alreadyRead << " --> " << (alreadyRead + batchSize) << std::endl;
-#endif
-    }
-
-    alreadyRead += std::cin.gcount();
-    buffer.resize(alreadyRead);
-
-    if (std::cin.fail() && !std::cin.eof()) [[unlikely]]
-    {
-        std::cerr << "Error reading input\n";
-        std::exit(1);
-    }
-}
-
-void read_stdio_into_buffer_unistd(std::vector<uint8_t>& buffer)
-{
     constexpr std::size_t FALLBACK_SIZE { 1024 };
 
     ssize_t alreadyRead = 0;
