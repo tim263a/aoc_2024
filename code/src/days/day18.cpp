47945de4ae1417aec93bdc68f7701374b89bdb02
Tim Gengenbach <tim.gengenbach@online.de>
2024-12-18 11:26:20 +0100

    day 18: store coordinates in 2d map

--- code/src/days/day18.cpp
+++ code/src/days/day18.cpp
@@ -2,6 +2,9 @@
 
 #include "util/print_fmt.h"
 #include "util/read_input.h"
+#include <cassert>
+#include <cstddef>
+#include <cstdlib>
 
 Day18::Day18()
     : m_buffer(32000)
@@ -32,7 +35,7 @@ void Day18::parseInput()
         int64_t x = readNumber(it);
         it++; // ","
         int64_t y = readNumber(it);
-        it++; // "\n"
+        // it++; // "\n"
 
         m_coordinates.emplace_back(x, y);
     }
@@ -40,9 +43,40 @@ void Day18::parseInput()
     printFmt("{} coordinates\n", m_coordinates.size());
 }
 
+#if 0 // real data
+constexpr std::size_t WIDTH = 71;
+constexpr std::size_t HEIGHT = 71;
+constexpr std::size_t FALLS = 1000;
+#else // example
+constexpr std::size_t WIDTH = 7;
+constexpr std::size_t HEIGHT = 7;
+constexpr std::size_t FALLS = 12;
+#endif
+
 uint64_t Day18::calculatePart1()
 {
     uint64_t sum = 0;
+
+    std::vector<uint8_t> map(WIDTH * HEIGHT, '.');
+
+    assert(m_coordinates.size() <= FALLS);
+
+    for (std::size_t i = 0; i < FALLS; i++)
+    {
+        auto pos = m_coordinates[i];
+        DEBUG_FMT("{} {} --> {}/{}\n",
+            pos.x, pos.y, pos.y * WIDTH + pos.x, map.size());
+        map[pos.y * WIDTH + pos.x] = 'X';
+    }
+
+    for (std::size_t y = 0; y < HEIGHT; y++)
+    {
+        for (std::size_t x = 0; x < WIDTH; x++)
+        {
+            printFmt("{:c}", map[y * WIDTH + x]);
+        }
+        printFmt("\n");
+    }
 
     return sum;
 }
