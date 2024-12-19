cdb25e99f53e67e43074081d02ee576c22496e45
Tim Gengenbach <tim.gengenbach@online.de>
2024-12-19 12:33:20 +0100

    day 19: parse input

--- code/api/days/day19.h
+++ code/api/days/day19.h
@@ -4,12 +4,16 @@
 
 #include <cstdint>
 
+#include <string>
 #include <vector>
 
 class Day19 final
     : public Day
 {
     std::vector<uint8_t> m_buffer;
+
+    std::vector<std::string> m_components;
+    std::vector<std::string> m_targets;
 
 public:
     Day19();
--- code/src/days/day19.cpp
+++ code/src/days/day19.cpp
@@ -4,12 +4,44 @@
 #include "util/read_input.h"
 
 Day19::Day19()
+    : m_buffer(32000)
 {
 
 }
 
+template<typename Iter>
+static std::string readString(Iter& it)
+{
+    auto itBegin = it;
+    while (*it >= 'a' && *it <= 'z')
+    {
+        it++;
+    }
+    return { itBegin, it };
+}
+
 void Day19::parseInput()
 {
+    read_stdio_into_buffer(m_buffer);
+
+    auto it = m_buffer.cbegin();
+    while (*it != '\n')
+    {
+        if (*it == ',')
+        {
+            it += 2; // ", "
+        }
+
+        m_components.push_back(readString(it));
+    }
+
+    it += 2;
+
+    while (*it != '\0')
+    {
+        m_targets.push_back(readString(it));
+        it++;
+    }
 }
 
 uint64_t Day19::calculatePart1()
